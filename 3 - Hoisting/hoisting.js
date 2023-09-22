6bbb70eb5a03e1388766c1314ce1ec2ad85318cc
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-09-22 12:53:00 -0700

    3 - Hoisting

---
+++ 3 - Hoisting/hoisting.js
@@ -0,0 +1,54 @@
+//CASE 1
+var x1 = 7
+
+function getName1() {
+    console.log('Hello Javascript!');
+}
+
+console.log('\n CASE-1');
+console.log(x1);
+console.log(getName1);
+getName1()
+
+
+//CASE 2
+console.log('\n CASE-2');
+console.log(x2);
+console.log(getName2);
+getName2()
+
+var x2 = 7
+
+function getName2() {
+    console.log('Hello Javascript!');
+}
+
+//CASE 3
+console.log('\n CASE-3');
+//console.log(y);
+console.log(getName3);
+getName3()
+
+function getName3() {
+    console.log('Hello Javascript!');
+}
+
+//CASE 4
+console.log('\n CASE-4');
+console.log(x);
+console.log(getName5);
+getName5()
+console.log(getName4);
+getName4()
+
+var x = 7;
+
+var getName4 = () => {
+    console.log('Hello Javascript!');
+}
+
+function getName5() {
+    console.log('Hello Javascript!');
+}
+
+
