78501ee2bfa77373278f7a71773b590a02e52d68
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-12-26 14:05:41 -0800

    event bubbling and capturing/trickling practice

---
+++ Core Javascript fundamentals/5 - Event Bubbling and Capturing(Trickling)/t1.js
@@ -0,0 +1,44 @@
+console.log("T1.js linked!");
+
+// Event Capturing / Trickling - 1st Priority
+//document.getElementById('gp').addEventListener('click', () => {
+//    console.log('GP clicked!');
+//}, useCapture = true)
+
+//document.getElementById('p').addEventListener('click', () => {
+//    console.log('P clicked!');
+//}, true)
+
+//document.getElementById('c').addEventListener('click', () => {
+//    console.log('C clicked!');
+//}, true)
+//GP -> P -> C
+
+
+// Event Bubbling
+//document.getElementById('gp').addEventListener('click', () => {
+//    console.log('GP clicked!');
+//})
+
+//document.getElementById('p').addEventListener('click', () => {
+//    console.log('P clicked!');
+//})
+
+//document.getElementById('c').addEventListener('click', () => {
+//    console.log('C clicked!');
+//})
+// C -> P -> GP
+
+
+document.getElementById('gp').addEventListener('click', () => {
+    console.log('GP clicked!');
+})
+
+document.getElementById('p').addEventListener('click', () => {
+    console.log('P clicked!');
+}, true)
+
+document.getElementById('c').addEventListener('click', () => {
+    console.log('C clicked!');
+})
+// Click Child, we get P,C,GP because E-C/T has first priority
