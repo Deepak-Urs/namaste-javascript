2311bdcfd5411becffd42f393815c09505b173d7
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-01-08 21:25:56 -0800

    advanced promise chaining and error handling

--- Namaste Javascript - part2/2 - Promises/promises-2.js
+++ Namaste Javascript - part2/2 - Promises/promises-2.js
@@ -1,34 +1,58 @@
 const cart = ['shoes', 'pants', 'kurta']
 
-const promise = createOrder(cart)
-promise
+createOrder(cart)
     .then(function (orderId) {
         console.log(orderId)
+        return orderId
     })
     .catch((err) => {
         console.log('Error seen - ', err.message)
     })
+    .then((orderId) => {console.log('createOrders output -->', orderId); return proceedToOrderPayment(orderId)})
+    .catch((err) => console.log(err.message))
+    .then((paymentInfo) => {console.log('proceedToOrderPayment output -->', paymentInfo); return showOrderSummary(paymentInfo)})
+    .catch((err) => console.log(err.message))
+    .then((orderSummary) => {console.log('showOrderSummary output -->', orderSummary); return updateWallet(orderSummary)})
+    .catch((err) => console.log(err.message))
+    .then(() => console.log('This promise block runs no matter what...'))
 
 // PROMISE CREATION function
 function createOrder(cart) {
     const pr = new Promise(function (resolve, reject) {
         // createOrder main logic -> validate cart , send back orderId
 
-        if(!validateCart(cart)) {
+        if (!validateCart(cart)) {
             const error = new Error("Cart is not valid")
             reject(error)
         }
 
         const orderId = '12345'
-        if(orderId) {
-            setTimeout(()=> resolve(orderId), 5000)
-
+        if (orderId) {
+            setTimeout(() => resolve(orderId), 2000)
         }
     })
 
     return pr
 }
 
+function proceedToOrderPayment(orderId) {
+    return new Promise(function(resolve, reject) {
+        resolve("Payment Successful");
+    })
+}
+
 function validateCart(cart) {
     return true
+}
+
+function showOrderSummary(paymentInfo) {
+    return new Promise(function (resolve, reject) {
+        resolve("Updated Order Summary")
+    })
+}
+
+function updateWallet(orderSummary) {
+    return new Promise(function (resolve, reject) {
+        resolve("Wallet Updated")
+    })
 }
